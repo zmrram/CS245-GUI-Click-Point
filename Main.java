6baa04940dc02bc01ace4b14aae00e3a901a55ba
Tin Huynh <tinhuynh@cpp.edu>
2017-04-09 16:17:05 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+/**
+ * *************************************************************
+ * file: GameFrame.java author: Tin Huynh & Eric Kuan class: CS 245 –
+ * Programming Graphical User Interfaces
+ *
+ * assignment: quarter project v1.3 
+ * date last modified: 8/23/2016
+ *
+ * purpose: This program stimulate a simple GUI of many mini games, mostly associate
+ * with clicking.
+ * **************************************************************
+ */
+package cs245.project;
+
+public class Main {
+
+    /**
+     * method: Main purpose: this method start the GUI
+     */
+    public static void main(String[] args) {
+        GameFrame game = new GameFrame();
+        game.setVisible(true);
+
+    }
+
+}
